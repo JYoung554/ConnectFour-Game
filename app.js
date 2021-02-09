b4b1c46a8f726ce5fec934780acf57705303a13e
JYoung554 <jontefears10@gmail.com>
2021-02-09 09:40:18 -0500

    Styled Game-Board backgroundand created game elements

---
+++ app.js
@@ -0,0 +1,5 @@
+const playerStyle = document.querySelector('.player')
+playerStyle.style.fontFamily = 'Helvetica, Arial, Sans-Serif'
+const currentPlayer = document.querySelector('player')
+const cells = document.querySelectorAll('game-grid')
+const result = document.querySelector('result')
