069bd378ff8af81c7bfa1f91c989e62017ec3409
JYoung554 <jontefears10@gmail.com>
2021-02-11 22:04:22 -0500

    Made Diagonal Win Outcomes

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const playerStyle = document.querySelector('.player')
 playerStyle.style.fontFamily = 'Helvetica, Arial, Sans-Serif'
-const displayCurrentPlayers = document.querySelector('player')
+const displayCurrentPlayers = document.querySelector('.player')
 const grid = document.querySelectorAll('game-grid')
 const cells = document.querySelectorAll('.cell')
 const topCells = document.querySelectorAll('.drop')
 // const row0 = document.querySelectorAll('.bottom')
 const result = document.querySelector('result')
 const resetButton = document.querySelector('.reset')
+const startGame = document.querySelector('.start')
 let currentPlayer = 'red'
 
 resetButton.style.backgroundColor = 'red'
 
 /////////////////////////////////////////////////////////////////////
-const gameActive = false
+let gameActive = false
 const gameWin = 0
 const winCombinations = [
   [0, 1, 2, 3],
@@ -60,7 +61,30 @@ const winCombinations = [
   [6, 13, 20, 27],
   [13, 20, 27, 34],
   [20, 27, 34, 41], //VERTICAL WINS
-  []
+  [14, 22, 30, 38],
+  [7, 15, 23, 31],
+  [15, 23, 31, 39],
+  [0, 8, 16, 24],
+  [8, 16, 24, 32],
+  [16, 24, 32, 40],
+  [1, 9, 17, 25],
+  [9, 17, 25, 33],
+  [17, 25, 33, 41],
+  [2, 10, 18, 26],
+  [10, 18, 26, 34],
+  [3, 11, 19, 27],
+  [21, 15, 9, 3],
+  [28, 22, 16, 10],
+  [22, 16, 10, 4],
+  [35, 29, 23, 17],
+  [29, 23, 17, 11],
+  [23, 17, 11, 5],
+  [36, 30, 24, 18],
+  [30, 24, 18, 12],
+  [24, 18, 12, 6],
+  [37, 31, 25, 19],
+  [31, 25, 19, 13],
+  [38, 32, 26, 20]
 ]
 
 let gameState = [
@@ -149,91 +173,141 @@ const columns = [column0, column1, column2, column3, column4, column5, column6]
 
 ///////////////////////////////////////////////////////
 const logic = function () {
+  gameActive = true
+  displayCurrentPlayers.innerText = `${currentPlayer}'s turn`
   for (let i = 0; i < topCells.length; i++) {
-    topCells[i].addEventListener('click', function (c) {
-      console.log(gameState)
-      if (!row5[i].classList.contains('full')) {
-        row5[i].style.backgroundColor = `${currentPlayer}`
-        console.log(row5[i].id)
-        gameState[35 + i] = currentPlayer
-        row5[i].classList.add('full')
-        switchPlayer()
-        return
-      } else if (!row4[i].classList.contains('full')) {
-        console.log('row4')
-        row4[i].style.backgroundColor = `${currentPlayer}`
-        gameState[28 + i] = currentPlayer
-        row4[i].classList.add('full')
-        switchPlayer()
-        return
-      } else if (!row3[i].classList.contains('full')) {
-        console.log('row3')
-        row3[i].style.backgroundColor = `${currentPlayer}`
-        gameState[21 + i] = currentPlayer
-        row3[i].classList.add('full')
-        switchPlayer()
-        return
-      } else if (!row2[i].classList.contains('full')) {
-        console.log('row2')
-        row2[i].style.backgroundColor = `${currentPlayer}`
-        gameState[14 + i] = currentPlayer
-        row2[i].classList.add('full')
-        switchPlayer()
-        return
-      } else if (!row1[i].classList.contains('full')) {
-        console.log('row1')
-        row1[i].style.backgroundColor = `${currentPlayer}`
-        gameState[7 + i] = currentPlayer
-        row1[i].classList.add('full')
-        switchPlayer()
-        return
-      } else if (!row0[i].classList.contains('full')) {
-        console.log('row0')
-        row0[i].style.backgroundColor = `${currentPlayer}`
-        gameState[i] = currentPlayer
-        row0[i].classList.add('full')
-        switchPlayer()
-        return
-      }
+    topCells[i].addEventListener('click', addLogic)
+  }
+  topCells.forEach((cell) => cell.classList.add(currentPlayer))
+}
 
-      console.log(gameState)
-    })
+const addLogic = (event) => {
+  let i = parseInt(event.target.id)
+  console.log(i)
+  if (!row5[i].classList.contains('full')) {
+    row5[i].style.backgroundColor = `${currentPlayer}`
+    console.log(row5[i].id)
+    gameState[35 + i] = currentPlayer
+    row5[i].classList.add('full')
+    checkWin()
+    return
+  } else if (!row4[i].classList.contains('full')) {
+    console.log('row4')
+    row4[i].style.backgroundColor = `${currentPlayer}`
+    gameState[28 + i] = currentPlayer
+    row4[i].classList.add('full')
+    checkWin()
+    return
+  } else if (!row3[i].classList.contains('full')) {
+    console.log('row3')
+    row3[i].style.backgroundColor = `${currentPlayer}`
+    gameState[21 + i] = currentPlayer
+    row3[i].classList.add('full')
+    checkWin()
+    return
+  } else if (!row2[i].classList.contains('full')) {
+    console.log('row2')
+    row2[i].style.backgroundColor = `${currentPlayer}`
+    gameState[14 + i] = currentPlayer
+    row2[i].classList.add('full')
+    checkWin()
+    return
+  } else if (!row1[i].classList.contains('full')) {
+    console.log('row1')
+    row1[i].style.backgroundColor = `${currentPlayer}`
+    gameState[7 + i] = currentPlayer
+    row1[i].classList.add('full')
+    checkWin()
+    return
+  } else if (!row0[i].classList.contains('full')) {
+    console.log('row0')
+    row0[i].style.backgroundColor = `${currentPlayer}`
+    gameState[i] = currentPlayer
+    row0[i].classList.add('full')
+    checkWin()
+    return
   }
 }
-logic()
 
-// const checkWin = () => {
-//   for (let i = 0; i < winCombos.length; i++) {
+const removeLogic = () => {
+  for (i = 0; i < topCells.length; i++) {
+    topCells[i].removeEventListener('click', addLogic)
+  }
+}
 
-//   }
-// }
+const checkWin = () => {
+  const reds = gameState
+    .map((color, index) => {
+      if (color === 'red') return index
+    })
+    .filter((val) => val !== undefined)
 
-//if win condition includes
+  const blues = gameState
+    .map((color, index) => {
+      if (color === 'blue') return index
+    })
+    .filter((val) => val !== undefined)
+
+  const isMatch = (array, target) => target.every((val) => array.includes(val))
+
+  console.log('reds: ', reds)
+  console.log('blues: ', blues)
+
+  for (let i = 0; i < winCombinations.length; i++) {
+    // console.log(...winCombinations[i])
+    if (isMatch(reds, winCombinations[i])) {
+      console.log('Red Wins!')
+      gameActive = false
+    }
+
+    if (isMatch(blues, winCombinations[i])) {
+      console.log('Blue Wins!')
+      gameActive = false
+    }
+  }
+  if (gameActive) {
+    switchPlayer()
+  } else {
+    displayCurrentPlayers.innerText = `${currentPlayer} Wins!`
+    removeLogic()
+  }
+}
 
 const switchPlayer = () => {
+  topCells.forEach((cell) => cell.classList.remove(currentPlayer))
   if (currentPlayer === 'red') {
     currentPlayer = 'blue'
   } else {
     currentPlayer = 'red'
   }
+  displayCurrentPlayers.innerText = `${currentPlayer}'s turn`
+  topCells.forEach((cell) => cell.classList.add(currentPlayer))
 }
 
-const display = () => {
-  if ((currentPlayer = 1)) {
-    displayCurrentPlayers = displayCurrentPlayers.innerText = `Player 1's Turn`
-  } else if ((currentPlayer = 2)) {
-    displayCurrentPlayers = displayCurrentPlayers.innerText = `Player 2's Turn`
-  }
-}
-
-// for (let i = 0; i < cells.length; i++) {
-//   cells[i].addEventListener('click', function () {
-//     console.log(topCells[i])
-//     console.log(cells[i])
-//   })
-// }
 resetButton.addEventListener('click', function () {
-  location.reload()
+  gameState = new Array(42).fill('')
+  cells.forEach((cell) => {
+    if (cell.classList.value.includes('full')) {
+      console.log(cell.classList.value)
+      cell.classList.remove('full')
+      cell.style.backgroundColor = 'none'
+    }
+  })
+  for (i = 0; i < topCells.length; i++) {
+    row5[i].style.backgroundColor = 'white'
+    row4[i].style.backgroundColor = 'white'
+    row3[i].style.backgroundColor = 'white'
+    row2[i].style.backgroundColor = 'white'
+    row1[i].style.backgroundColor = 'white'
+    row0[i].style.backgroundColor = 'white'
+  }
+  console.log(gameState)
+  logic()
+})
+
+startGame.addEventListener('click', () => {
+  logic()
+  startGame.style.display = 'none'
 })
 
 // document.querySelector('.switch').addEventListener('click', switchPlayer)
